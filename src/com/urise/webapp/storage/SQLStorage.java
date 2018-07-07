0fe7cc22fe8055ffbca58fb114a28573401fe7fd
Maria <mrozhavskaya@gmail.com>
2018-07-07 16:15:52 +0200

    corrected

--- src/com/urise/webapp/storage/SQLStorage.java
+++ src/com/urise/webapp/storage/SQLStorage.java
@@ -9,14 +9,11 @@ import org.postgresql.util.PSQLException;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class SQLStorage implements Storage {
 
-    private final SQLHelper<Resume> resumeHelper;
-    private final SQLHelper<List<Resume>> listHelper;
-    private final SQLHelper<Integer> intHelper;
+    private final SQLHelper helper;
 
     private final static String CLEAR_QUERY;
     private final static String GET_QUERY;
@@ -32,24 +29,22 @@ public class SQLStorage implements Storage {
         UPDATE_QUERY = "UPDATE resume SET full_name = ? WHERE uuid =?";
         SAVE_QUERY = "INSERT INTO resume (uuid, full_name) VALUES (?,?)";
         DELETE_QUERY = "DELETE FROM resume where uuid=?";
-        GET_ALL_QUERY = "SELECT * FROM resume";
+        GET_ALL_QUERY = "SELECT * FROM resume ORDER BY uuid";
         SIZE_QUERY = "SELECT COUNT(*) AS total FROM resume";
     }
 
     public SQLStorage(String dbUrl, String dbUser, String dbPassword) {
-        resumeHelper = new SQLHelper<Resume>(dbUrl, dbUser, dbPassword);
-        listHelper = new SQLHelper<List<Resume>>(dbUrl, dbUser, dbPassword);
-        intHelper = new SQLHelper<Integer>(dbUrl, dbUser, dbPassword, PreparedStatement::executeUpdate);
+        helper = new SQLHelper(dbUrl, dbUser, dbPassword);
     }
 
     @Override
     public void clear() {
-        intHelper.help(CLEAR_QUERY);
+        helper.help(CLEAR_QUERY);
     }
 
     @Override
     public Resume get(String uuid) {
-        return resumeHelper.help(ps -> {
+        return helper.help(ps -> {
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
@@ -60,13 +55,13 @@ public class SQLStorage implements Storage {
 
     @Override
     public void update(Resume r) {
-        if (intHelper.help(UPDATE_QUERY, r.getFullName(), r.getUuid()) == 0)
+        if (helper.help(UPDATE_QUERY, r.getFullName(), r.getUuid()) == 0)
             throw new NotExistStorageException("there is no such resume");
     }
 
     @Override
     public void save(Resume r) {
-        intHelper.help(pr -> {
+        helper.help(pr -> {
             try {
                 return pr.executeUpdate();
             } catch (PSQLException e) {
@@ -78,45 +73,36 @@ public class SQLStorage implements Storage {
 
     @Override
     public void delete(String uuid) {
-        if (intHelper.help(DELETE_QUERY, uuid) == 0)
+        if (helper.help(DELETE_QUERY, uuid) == 0)
             throw new NotExistStorageException("there is no such uuid");
     }
 
     @Override
     public List<Resume> getAllSorted() {
-        List<Resume> list = listHelper.help(ps -> {
-                    ResultSet rs = ps.executeQuery();
-                    List<Resume> rList = new ArrayList<>();
-                    while (rs.next()) {
-                        rList.add(new Resume(rs.getString("uuid"), rs.getString("full_name")));
-                    }
-                    return rList;
-                }
-                , GET_ALL_QUERY, null);
-
-        Collections.sort(list);
-        return list;
+        return helper.help(ps -> {
+            ResultSet rs = ps.executeQuery();
+            List<Resume> rList = new ArrayList<>();
+            while (rs.next()) {
+                rList.add(new Resume(rs.getString("uuid"), rs.getString("full_name")));
+            }
+            return rList;
+        }, GET_ALL_QUERY);
     }
 
     @Override
     public int size() {
-        return intHelper.help(ps -> {
+        return helper.help(ps -> {
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new StorageException("result set is empty");
             }
             return rs.getInt("total");
-        }, SIZE_QUERY, null);
+        }, SIZE_QUERY);
     }
 
-    public static class SQLHelper<T> {
+    public static class SQLHelper {
         private final ConnectionFactory connectionFactory;
-        private Helper<T> helper;
-
-        SQLHelper(String dbUrl, String dbUser, String dbPassword, Helper<T> helper) {
-            this(dbUrl, dbUser, dbPassword);
-            this.helper = helper;
-        }
+        private static final Helper<Integer> INT_HELPER = PreparedStatement::executeUpdate;
 
         SQLHelper(String dbUrl, String dbUser, String dbPassword) {
             connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
@@ -126,19 +112,19 @@ public class SQLStorage implements Storage {
             K doHelp(PreparedStatement ps) throws SQLException;
         }
 
-        T help(Helper<T> helper, String query) {
+        <T> T help(Helper<T> helper, String query) {
             return this.help(helper, query, null);
         }
 
-        T help(String query, String... parameters) {
-            return this.help(helper, query, parameters);
+        Integer help(String query, String... parameters) {
+            return this.help(INT_HELPER, query, parameters);
         }
 
-        T help(String query) {
-            return this.help(helper, query);
+        Integer help(String query) {
+            return this.help(INT_HELPER, query);
         }
 
-        T help(Helper<T> helper, String query, String... parameters) {
+        <T> T help(Helper<T> helper, String query, String... parameters) {
             try (Connection conn = connectionFactory.getConnection();
                  PreparedStatement ps = conn.prepareStatement(query)) {
                 if (parameters != null) {
